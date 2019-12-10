e352f5b2eba4b7caa6c8c84f37e4872de1bfd51a
narayn92 <2007.narayanan@gmail.com>
2019-12-10 13:45:02 +0530

    azure deployment - linux web app with node express static files

---
+++ azure/server.js
@@ -0,0 +1,17 @@
+var express = require("express");
+//var bodyParser = require("body-parser");
+//var mongodb = require("mongodb");
+//var ObjectID = mongodb.ObjectID;
+
+//var CONTACTS_COLLECTION = "contacts";
+
+var app = express();
+//app.use(bodyParser.json());
+
+// Create link to Angular build directory
+var distDir = __dirname + "/dist";
+app.use(express.static(distDir));
+
+
+app.listen(process.env.PORT || 5000);
+// Rest of server.js code below
